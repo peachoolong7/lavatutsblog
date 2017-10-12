7747ec4fcf0bdda68a0e91766888ce701ac1776d
transzero001 <bryancomander@gmail.com>
2017-10-12 11:18:45 +0700

    talala

---
+++ resources/views/auth/password/email.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AM-PC
+ * Date: 10/10/2017
+ * Time: 5:50 PM
+ */
---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AM-PC
+ * Date: 10/12/2017
+ * Time: 11:06 AM
+ */
